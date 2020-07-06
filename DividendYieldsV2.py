ee868ed26509fcdd69303864303a451c8b6fb8cc
Joe Caswell <fsujoseph@gmail.com>
2020-07-06 13:32:40 -0400

    Updated comments

--- DividendYieldsV2.py
+++ DividendYieldsV2.py
@@ -1,10 +1,16 @@
+# Author: Joe Caswell
+# Date: 6/8/2020
+# Description: This program prompts the user to create a new portfolio file containing all of their stocks, or load
+# an existing one. This program simply functions as an add-on if you are curious to see the amount of dividends
+# your investments are producing each year.
+
 import os
 import json
 import requests
 import bs4
 
-# Obtain  response from user
-def yn():
+
+def yn():   # Obtain yes/no response from user
     while True:
         ans = input()
         if ans.lower() == "yes":
@@ -14,72 +20,74 @@ def yn():
         else:
             print("Please answer with yes or no.")
 
-# Guides user to login with previously saved profile or create new one
+
 def login():
-    # Lists profiles in folder
-    entries = os.listdir()
+    """
+    This function creates or loads in a user's stock portfolio as a .json file.
+    """
+    entries = os.listdir()  # Lists profiles in folder
+
     # Checks if user is new or has profile
     print("Are you a returning user?")
     check = yn()
-    # Requests users name to find profile or to create new one
-    name = input("What is your name? ")
-    # Formats name to later create it in proper format
-    name_form = name.lower().replace(" ","")+".json"
 
-    # Checks to make sure profile does not already exist
-    if check is False and name_form in entries:
+    name = input("What is your name? ")     # Requests user's name to find profile or to create new one
+    name_formatted = name.lower().replace(" ", "")+".json"   # Formats name to later be opened and edited
+
+    if check is False and name_formatted in entries:    # Checks to make sure profile does not already exist
         print("Your portfolio already exists! Please try again.")
-        quit()
-    # Asks if user would like to make a new profile, otherwise ends program
-    elif check is False:
+        print("These are the profiles that are already created: ")
+        print(entries)
+        access()
+    elif check is False:    # Asks if user would like to make a new profile, otherwise ends program
         print("Would you like to create a new portfolio? ")
         ans = yn()
         if ans is False:
             quit()
-        # Creates user profile with name formatted as .json file and initializes and empty dictionary
-        else:
-            print("A new portfolio has been created called {}".format(name_form))
-            file = open(name_form,'w')
-            dic = {}
-            json.dump(dic,file)
+        else:   # Creates user profile with name formatted as .json file
+            print("A new portfolio has been created called {}".format(name_formatted))
+            file = open(name_formatted, 'w')     # Creates file
+            dic = {}    # Empty dictionary which will contain stock name and price
+            json.dump(dic, file)     # json allows dictionary to be stored in a text file (otherwise not possible)
             file.close()
-            return name_form
-    # If user is returning
-    else:
-        if name_form in entries:
+            return name_formatted
+    else:   # If user is returning
+        if name_formatted in entries:
             print("Welcome back {}!".format(name))
-            return name_form
-        # If user enters profile that does not exist asks to create new one, otherwise kill program and show files
-        else:
+            return name_formatted
+        else:   # If user enters profile that does not exist asks to create new one, otherwise kill program
             print("Sorry, your portfolio is not in the files. Would you like to create a new portfolio? ")
             ans = yn()
             if ans is True:
-                file = open(name_form, 'w')
+                file = open(name_formatted, 'w')
                 dic = {}
                 json.dump(dic, file)
                 file.close()
-                return name_form
+                return name_formatted
             else:
                 print("These are the profiles that are already created: ")
                 print(entries)
                 quit()
 
-def data_entry():
-    # Calls login script to obtain file name
-    file_name = login()
 
-    # Loads file into variable
+def data_entry():
+    """
+    This function allows the user to first view their profile. Then it contains options to add, remove, or change
+    the stock holdings.
+    """
+    file_name = login()     # Calls login script to obtain file name
+
     file = open(file_name, 'r')
-    portf = json.load(file)
+    portfolio = json.load(file)     # Loads file into variable
     file.close()
 
     # Allows user to see stocks in portfolio
     print("Would you like to view your portfolio?")
     ans = yn()
     if ans is True:
-        for key in portf:
-            print("{}:{}".format(key, portf[key]))
-        if len(portf) == 0:
+        for key in portfolio:
+            print("{}:{}".format(key, portfolio[key]))
+        if len(portfolio) == 0:
             print("Your portfolio is empty!")
     else:
         pass
@@ -91,27 +99,30 @@ def data_entry():
         print("Would you like to add stocks? ")
         ans = yn()
         if ans is True:
-            portf = add_stock(portf)
+            portfolio = add_stock(portfolio)
         else:
             pass
         print("Would you like to remove stocks? ")
         ans = yn()
         if ans is True:
-            portf = remove_stock(portf)
+            portfolio = remove_stock(portfolio)
         else:
             pass
         print("Would you like to change your number of shares? ")
         ans = yn()
         if ans is True:
-            portf = change_stock(portf)
-            return portf, file_name
+            portfolio = change_stock(portfolio)
+            return portfolio, file_name
         else:
-            return portf, file_name
+            return portfolio, file_name
     else:
-        return portf, file_name
+        return portfolio, file_name
+
 
 def add_stock(portf):
-    # Allows user to add stocks and quantities to portfolio
+    """
+    This function contains the process for adding a stock to a profile.
+    """
     while True:
         stock = (input("What is the name of the stock (shortened version)? Type done when you are finished. ")).upper()
         if stock.lower() == "done":
@@ -122,16 +133,18 @@ def add_stock(portf):
         else:
             while True:
                 shares = input("How many shares of {} do you own? ".format(stock))
-                try:
-                    shares = int(shares)
+                if float(shares) > 0:
                     break
-                except ValueError:
-                    print("Please enter an integer. ")
+                else:
+                    print("Please enter a positive number. ")
                     pass
-        portf[stock] = shares
+            portf[stock] = shares
+
 
 def remove_stock(portf):
-    # Allows user to remove stocks
+    """
+    This function contains the process for removing a stock to a profile.
+    """
     while True:
         remove = input("Please enter the stock you would like to remove. Type done when you are finished. ")
         if remove.lower() == 'done':
@@ -142,8 +155,11 @@ def remove_stock(portf):
             print("{} is not a part of your portfolio! ".format(remove))
             pass
 
+
 def change_stock(portf):
-    # Allows user to adjust stock shares
+    """
+    This function contains the process for changing the stock quantity of a profile.
+    """
     while True:
         change = input("Please enter the stock you would like to change. Type done when you are finished. ")
         if change.lower() == 'done':
@@ -152,32 +168,32 @@ def change_stock(portf):
             print("{} is not a part of your portfolio! ".format(change))
             pass
         elif change.upper() in portf:
-            while True:
-                shares = input("How many shares of {} do you own? ".format(change))
-                try:
-                    shares = int(shares)
-                    break
-                except ValueError:
-                    print("Please enter an integer. ")
-                    pass
+            shares = input("How many shares of {} do you own? ".format(change.upper()))
             portf[change.upper()] = shares
 
-# Access stocks
+
 def access():
-    portfname = data_entry()
-    portf = portfname[0]
-    file_name = portfname[1]
+    """
+    This function contains the process for scraping the stock price data from Robinhood and then printing the
+    dividend payout.
+    """
+    portfolio_and_file_name = data_entry()
+    portfolio = portfolio_and_file_name[0]
+    file_name = portfolio_and_file_name[1]
 
     dividends = 0
     value = 0
-    for stock, shares in portf.items():
-        link = 'https://robinhood.com/stocks/'+stock
+    for stock, shares in portfolio.items():
+        link = 'https://robinhood.com/stocks/'+stock    # Creates link to Robinhood specific to stock name
         r = requests.get(link)
-        soup = bs4.BeautifulSoup(r.text, "lxml")
+        soup = bs4.BeautifulSoup(r.text, "lxml")    # Loads text data from webpage
 
-        price = soup.find('div',{'class':'QzVHcLdwl2CEuEMpTUFaj'}).text
+        # The following methods work by finding out where the dividend yield is located on the webpage. Different
+        # Stocks have different things available which changes the location of the dividend yield. I tested it out
+        # for stocks and different ETF's so every scenario should be covered.
+        price = soup.find('div', {'class': 'QzVHcLdwl2CEuEMpTUFaj'}).text
         price = price[1:]
-        print('{} Price: ${}'.format(stock,price))
+        print('{} Price: ${}'.format(stock, price))
 
         div = soup.find_all('div', {'class': '_2SYphfY1DF71e5bReqgDyP'})[6].text
         div = div[14:]
@@ -195,8 +211,11 @@ def access():
         if div == 0:
             print('{} Div: N/A'.format(stock))
         else:
-            print('{} Div: {}%'.format(stock,div))
+            print('{} Div: {}%'.format(stock, div))
 
+        price = price.replace(',', '')   # If the stock price is over 4 figures
+
+        # Rung through stocks class within this for loop to get the dividends and share values
         info = Stocks(stock, shares, price, div)
         dividends += info.dividends()
         value += info.value()
@@ -204,16 +223,21 @@ def access():
     print("Your total yearly dividends is ${:.2f}".format(dividends))
     print("Your total portfolio is worth ${:.2f}".format(value))
 
+    # Dumps the updated portfolio back into the .json file
     file = open(file_name, 'w+')
-    json.dump(portf, file)
+    json.dump(portfolio, file)
     file.close()
 
+
 class Stocks:
-    def __init__(self,stock,shares,price,div):
+    """
+    This class contains methods to return the dividend and share value
+    """
+    def __init__(self, stock, shares, price, div):
         self.stock = stock
         self.shares = float(shares)
         self.price = float(price)
-        self.div= float(div)
+        self.div = float(div)
 
     def dividends(self):
         dividend = self.div*self.price*self.shares/100
@@ -222,7 +246,6 @@ class Stocks:
     def value(self):
         value = self.shares*self.price
         return value
-
 
 
 access()
